610f19f8482510326fcd5f368d4cd9e52b83e0d8
Kareem_Mostafa <kareemmostafa114@gmail.com>
2023-01-11 18:27:20 +0200

    pagination done

--- JS/home.js
+++ JS/home.js
@@ -15,3 +15,34 @@ async function getLatestOrder(){
 }
 
 getLatestOrder();
+
+
+
+async function reqPage (p){
+    let sentDATA =new FormData();
+    sentDATA.append("pageno",`${p.innerHTML}`);
+    let res=await fetch ("http://localhost/php/getProductByPage2.php",{
+        method:"POST",
+        body:sentDATA,
+    })
+    let d=await res .json();
+    console.log(d);
+}
+
+
+async function getNoOfPages()
+{
+    let response=await fetch("http://localhost/php/AllProducts2.php");
+    let data=await response.json() ;
+    for (let i=0;i<data;i++)
+    {
+        let p=document.createElement("p");
+        p.innerHTML=i+1;
+        document.querySelector(".change-product").append(p);
+        p.addEventListener("click",()=>{
+            reqPage(p);
+        })
+    } 
+}
+
+getNoOfPages();
