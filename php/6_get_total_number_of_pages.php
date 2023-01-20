a71ad407690b2af40ceb9d56a44828858ff2a667
mohamed-sarhan <mohsarhan88@gmail.com>
2023-01-20 23:32:11 +0200

    v1.1

--- php/6_get_total_number_of_pages.php
+++ php/6_get_total_number_of_pages.php
@@ -4,7 +4,7 @@ require_once("./env.php");
 require_once("./checksDB.php");
 // echo "sadasd";
 // phpinfo();
-$page = $_POST['page'];
+// $page = $_POST['page'];
 // print_r(json_encode($id));
 $db = new checks($DATABASE, $DATABASE_HOST, $DATABASE_NAME, $DATABASE_USERNAME, $DATABASE_PASSWORD);
 // $db = new checks('mysql', 'localhost', 'cafe', "root", "12345678");
